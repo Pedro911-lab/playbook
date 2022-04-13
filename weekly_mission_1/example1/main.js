03deb10bd4e47800f4b1ab5fff0bd25bb14ea94b
Pedro911-lab <esqcorp1@gmail.com>
2022-04-12 23:07:38 -0500

    semana_1

--- weekly_mission_1/example1/main.js
+++ weekly_mission_1/example1/main.js
@@ -3,7 +3,7 @@
 let myCar = new Object(); // Creación de un objeto
 myCar.make = 'Ford'; // Guardando un valor dentro del objeto creado
 myCar.model = 'Mustang';
-myCar.year = 1969;
+myCar.year = 1970;
 
 console.log(myCar) // Imprimiendo objeto
 
